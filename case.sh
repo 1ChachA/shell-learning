2d4f43165938013b8e9877cfe7b7f8187e9f84c1
1005808270@qq.com <112203qq@>
2022-09-04 13:56:47 +0800

    hello

---
+++ case.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+case $1 in 
+	"1")
+		echo "input 1"
+	;;
+	"2")
+		echo "input 2"
+	;;
+	"3")
+		echo "input 3"
+	;;
+	*)
+		echo "input other"
+	;;
+esac
---
+++ if.sh
@@ -0,0 +1,6 @@
+if [ $1 -eq 5 ]
+then
+	echo "BIGGER"
+else
+	echo "LESSER"
+fi
